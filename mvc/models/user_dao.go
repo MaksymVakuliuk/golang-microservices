7d6a00db630f9d0521a8145f18c3c3b4e91184ca
Maksym Vakuliuk <vakuliukmaksym@gmail.com>
2021-05-23 21:10:53 +0300

    Fixed structure of project. Added user_dao and changed GetUser func in users_services.

---
+++ mvc/models/user_dao.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/MaksymVakuliuk/golang-microservices/mvc/utils"
+)
+
+var (
+	users = map[uint64]*User{
+		123: {Id: 123, FirstName: "Ivan", SecondName: "Ivanov", Email: "iviv@gmail.com"},
+	}
+)
+
+func GetUser(userId uint64) (*User, *utils.ApplicationError) {
+	if user := users[userId]; user != nil {
+		return user, nil
+	}
+	return nil, &utils.ApplicationError{
+		Message:    fmt.Sprintf("user %v was not found", userId),
+		StatusCode: http.StatusNotFound,
+		Code:       "not found",
+	}
+}
