43f60a3cd45569d8d4ff2320c36878353fcb3f99
Maksym Vakuliuk <vakuliukmaksym@gmail.com>
2021-05-23 20:38:22 +0300

    Implemented a straightforward MVC pattern

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MaksymVakuliuk/golang-microservices
+
+go 1.16
---
+++ mvc/models/user.go
@@ -0,0 +1,8 @@
+package models
+
+type User struct {
+	Id         uint64 `json:"id"`
+	FirstName  string `json:"first_name"`
+	SecondName string `json:"second_name"`
+	Email      string `json:"email"`
+}
